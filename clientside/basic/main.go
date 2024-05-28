80eee22bf19e8b5b23ad215bc10bf81fd261724f
jimmydesu <jimsyyap@gmail.com>
2024-05-28 22:33:26 +1000

    clientside/basic

---
+++ clientside/basic/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+func main() {
+
+	resp, err := http.Get("https://www.google.com/robots.txt")
+	if err != nil {
+		log.Panicln(err)
+	}
+	// Print HTTP Status
+	fmt.Println(resp.Status)
+
+	// Read and display response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Println(string(body))
+	resp.Body.Close()
+
+	resp, err = http.Head("https://www.google.com/robots.txt")
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
+
+	form := url.Values{}
+	form.Add("foo", "bar")
+	resp, err = http.Post(
+		"https://www.google.com/robots.txt",
+		"application/x-www-form-urlencoded",
+		strings.NewReader(form.Encode()),
+	)
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
+
+	req, err := http.NewRequest("DELETE", "https://www.google.com/robots.txt", nil)
+	if err != nil {
+		log.Panicln(err)
+	}
+	var client http.Client
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
+
+	req, err = http.NewRequest("PUT", "https://www.google.com/robots.txt", strings.NewReader(form.Encode()))
+
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Panicln(err)
+	}
+	resp.Body.Close()
+	fmt.Println(resp.Status)
+
+}
+
+/*
+* this snippet shows how to
+* perform different types of http requests (get, head, post, delete and put)
+* print the http status code for each requests
+* handle the response body
+* log errors and close response bodies to free up resources
+* these are examples of how to use go's net/http package to interact with web servers through various http methods
+ */
---
+++ clientside/notes.md
@@ -0,0 +1,5 @@
+this chapter focuses on the client side. It will first introduce you to the basics of building and customizing http requests and receiving their responses. Then you'll learn how to parse structured response data so the client can interrogate the information to determine actionable or relevant data.
+
+Finally, you'll learn how to apply these fundamentals by building http clients that interact with a variety of security tools and resources.
+
+The clients you develop will query and consume the APIs of shodan, bing, and metasploit and will search and parse document metadata in a manner similar to the metadata serch tool FOCA.
