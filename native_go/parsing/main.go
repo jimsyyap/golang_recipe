1553c9178a98c391027d24b131bbe20383e9bdf4
jimmydesu <jimsyyap@gmail.com>
2024-05-31 11:12:12 +1000

    apps folder created

--- native_go/parsing/main.go
+++ native_go/parsing/main.go
@@ -21,3 +21,11 @@ func main() {
 
 	ast.Print(fset, f)
 }
+
+/*
+* this code takes a simple go program and shos you the building blocks it's made of.
+* it's like a toy x-ray machine for looking at other toys to see how they work inside.
+* by understanding how other go programs are written, you can learn to write your own go programs better.
+* this is also a starting point for building tools that can automatically fix mistakes in code, change how code looks, or even create new programs based on patterns in existing code
+*
+ */
