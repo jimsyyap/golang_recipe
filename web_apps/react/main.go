db32fa393fe508bc933e9e3be81ff11f425542aa
jimmydesu <jimsyyap@gmail.com>
2024-07-14 10:16:42 +1000

    pivot in july

---
+++ web_apps/react/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+)
+
+func main(){
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+
+    app.Get("/json", func(c *fiber.Ctx) error {
+        return c.JSON(fiber.Map{
+            "message": "Hello, World!",
+        })
+    })
+
+    app.Listen(":3000")
+}
